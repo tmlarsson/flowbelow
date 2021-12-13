74e87a1af5c3a99b543cb24dc37cbb986843551b
tmlarsson <timakelarsson@gmail.com>
2021-12-13 15:46:40 +0100

    Moved "/simulations" to homepage.

--- dbt_pg/urls.py
+++ dbt_pg/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.map_homepage, name='Homepage'),
+    path('', views.map_simulation, name='Homepage'),
     path('about', views.about, name='About us'),
-    path('simulations', views.map_simulation, name='Simulations'),
 ]
