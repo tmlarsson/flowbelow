676228bb2b030e3659bb68c85856bf47161cfb5e
tmlarsson <timakelarsson@gmail.com>
2021-12-15 12:24:28 +0100

    Removed all unused files.

--- dbt_pg/views.py
+++ dbt_pg/views.py
@@ -2,15 +2,6 @@ from django.shortcuts import render
 from .models import Sensor as S
 from .models import Sensor_time_value as ST
 from .models import raster_data as R
-import datetime
-
-def map_homepage(request):
-    context = {
-        'timeData': ST.objects.all(),
-        'sensor_objects': S.objects.all(),
-        'rasterObjects': R.objects.all()
-    }
-    return render(request, 'map.html', context)
 
 def about(request):
     return render(request, 'about.html')
